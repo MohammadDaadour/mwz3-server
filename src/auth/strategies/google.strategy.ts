70709521f06d9b06d2844f070da41fecd5f35ba4
MohammadDaadour <daadourmohammad@gmail.com>
2026-08-04 13:55:17 +0300

    commented state:true

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -12,7 +12,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       callbackURL: `https://mwz3.com/redirect/google`,
       scope: ['profile', 'email'],
       passReqToCallback: true,
-      state: true,
+      // state: true,
     });
   }
 
